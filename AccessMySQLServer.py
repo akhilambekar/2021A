ef9f03cce8e8abd3231abe66f323dcf38a06086b
akhilambekar <42722868+akhilambekar@users.noreply.github.com>
2021-03-11 14:49:45 -0600

    Python code to connect the database server and execute queries.
    Python code that successfully makes a connection to the database server and can execute the required queries.

---
+++ AccessMySQLServer.py
@@ -0,0 +1,49 @@
+#Import Required modules
+import base64
+from sshtunnel import SSHTunnelForwarder
+import pymysql
+
+#Password encryption/decryption for accessing Database server
+#EncodedServerPass = base64.b64encode("ServerPass".encode("utf-8"))
+#EncodedMySQLPass = base64.b64encode("MySQLPass".encode("utf-8"))
+with open("EncodedPasswords.txt", "r") as filestream:
+    for line in filestream:
+            EncServerPass, EncMySQLPass = line.split(",")
+
+
+ServerPass = base64.b64decode(EncServerPass)
+MySQLPass = base64.b64decode(EncMySQLPass)
+
+# SSH linux server
+server = SSHTunnelForwarder(
+    '66.76.242.194',
+    ssh_username='ambekarakhil',
+    ssh_password= ServerPass,
+    remote_bind_address=('127.0.0.1', 3306)
+)
+server.start()
+
+#Make a database connection
+cnx = pymysql.connect(
+    host='127.0.0.1',
+    port=server.local_bind_port,
+    user='ambekarakhil',
+    password= MySQLPass,
+    db='ambekarakhil'
+)
+
+print("Connected to the following MySQL Server: " + cnx.get_server_info())
+
+#Execute SQL Commands
+with cnx.cursor() as cur:
+    
+    cur.execute('use ambekarakhil;')
+    cur.execute('Select * from verus0302c;')
+    rows = cur.fetchall()
+    for row in rows:
+        print(f'{row[0]} {row[1]} {row[2]}')
+
+
+# Close all connections
+cnx.close()
+server.stop()
