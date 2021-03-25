b08ef48be4700a690daba0926faa8eff195f4b8a
akhilambekar <42722868+akhilambekar@users.noreply.github.com>
2021-03-25 16:10:59 -0500

    Update GetJSONData_NLPParser.py
    Added a comment to test automatic git pull operation.

--- GetJSONData_NLPParser.py
+++ GetJSONData_NLPParser.py
@@ -14,5 +14,6 @@ data = '{"text":InputString = '+ InputString + '}'
 
 response = requests.post('http://66.76.242.198:9888/', data=data).json()
 
+#Adding a test comment to check if the automatic git pull is working or not
 
 #print(json.dumps(response, indent=4, sort_keys=True))
