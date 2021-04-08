90a3f9449ba301de9e0021633020a3096633a513
akhilambekar <42722868+akhilambekar@users.noreply.github.com>
2021-04-08 09:21:47 -0500

    Added more features
    Display tree feature added by Feiyi
    The results now show formatted StanfordNLP results in the form of a table.
    The input Sentence is now saved in the database.

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
-
+from bs4 import BeautifulSoup
+from flask import render_template
 import requests
 import json
 import base64
+from sshtunnel import SSHTunnelForwarder
 import pymysql
 from pymysql.converters import escape_string
 
@@ -16,11 +18,17 @@ from json2html import *
 
 from collections import defaultdict
 
+from nltk.tree import ParentedTree
+from nltk.treeprettyprinter import TreePrettyPrinter
+import re
+
+import pandas as pd
+from tabulate import tabulate
+
 
 
 app = dash.Dash(__name__)   #external_stylesheets=[dbc.themes.SUPERHERO]
 
-appserver = app.server
 
 graph_card = dbc.Card([
     
@@ -30,6 +38,7 @@ graph_card = dbc.Card([
             className="mb-3", placeholder="Please inter a statement in proper grammar with all implied clauses."),
                             ])
                     ])
+
 
 app.layout = html.Div([
     
@@ -65,7 +74,11 @@ app.layout = html.Div([
     dbc.Row([dbc.Col(graph_card, width=8)], justify="around"),
     
     dcc.ConfirmDialogProvider( html.Button('Submit to sNLP', style={"height": "auto", "margin-left": 314}),id='submit_button'), #message='Danger danger! Are you sure you want to continue?'
-    dbc.Row([html.Div(id='textoutput', style={'whiteSpace': 'pre-line','margin-left': 320})])
+   # dbc.Row([html.Div(id='textoutput', style={'whiteSpace': 'pre-line','margin-left': 320, 'width': '1000px', 'display' : "inline-block !important"})])
+    html.Br(),
+    
+    html.Div(id='textoutput', style={'whiteSpace': 'pre', 'margin-left': 320, 'width': '1000px', "font-family": "monospace"})
+
 ])
 
 
@@ -123,10 +136,11 @@ def text_output(submit_n_clicks,value, name, email, phone):
         MySQLUserName = 'ambekarakhil'
         DatabaseName = 'ambekarakhil'
 
+
         #Make a database connection
         cnx = pymysql.connect(
             host='127.0.0.1',
-            port=3306,
+            port=server.local_bind_port,
             user= MySQLUserName,
             password= MySQLPass,
             db = DatabaseName
@@ -141,13 +155,14 @@ def text_output(submit_n_clicks,value, name, email, phone):
         TeacherVal = name
         AssertionVal= "dummyAssertion"
         LinkageVal = "dummyLinkage"
+        InputSentence = value
 
         #Execute SQL Commands
         with cnx.cursor() as cur:
     
             cur.execute('use ambekarakhil;')
-            sql_command = """INSERT INTO verus0302c(count, teacher, assertion, nlp, linkages) VALUES (%s, %s, %s, %s, %s)""" 
-            cur.execute(sql_command, (CountVal, TeacherVal, AssertionVal, json_data_sql, LinkageVal))
+            sql_command = """INSERT INTO verus0302c(count, teacher, assertion, nlp, linkages, inputSen) VALUES (%s, %s, %s, %s, %s, %s)""" 
+            cur.execute(sql_command, (CountVal, TeacherVal, AssertionVal, json_data_sql, LinkageVal, InputSentence))
 
         cnx.commit()
 
@@ -164,14 +179,65 @@ def text_output(submit_n_clicks,value, name, email, phone):
         """
         # Close all connections
         cnx.close()
+        server.stop()
 
+        # # Convert the JSON result in a tabular format with HTML tags
+        # jsontohtml = json2html.convert(json = Json_data);
+
+        # #---------------Will expand this code to add more complex tasks --------------------
+        # # Display the HTML code in a new browser
+        # import webbrowser
+        # f = open('JSONResult.html','w')
+        # f.write(jsontohtml)
+        # f.close()
+
+        # webbrowser.open_new_tab('JSONResult.html')
+        # #---------------Will expand this code to add more complex tasks --------------------
+
+        # Flatten nested JSON data and convert the resultant into a dataframe.
+        # This dataframe will be displayed on the web page.
         
-        return "submitted {} times; user info: [ Name: {} | Email: {} | Phone Number: {} ]".format(submit_n_clicks, name, email, phone)
-
-        # Convert the JSON result in a tabular format with HTML tags
-        jsontohtml = json2html.convert(json = Json_data);
+        df_nested_list = pd.json_normalize(Json_data)
+        df_nested_Sen = pd.json_normalize(Json_data, record_path = ["sentences"])
+        df_nested_3 = df_nested_Sen.iloc[:, 0:2]
+        df_nested_basic = df_nested_Sen.iloc[:, 2:3]
+        df_nested_enhanced = df_nested_Sen.iloc[:, 3:4]
+        df_nested_enhancedPP = df_nested_Sen.iloc[:, 4:5]
+        df_nested_entity = df_nested_Sen.iloc[:, 5:6]
+        df_nested_tokens = df_nested_Sen.iloc[:, 6:7]
+        df_nested_basic['basicDependencies'] = df_nested_basic['basicDependencies'].astype('str').str.replace('},','}\n')
+        df_nested_enhanced['enhancedDependencies'] = df_nested_enhanced['enhancedDependencies'].astype('str').str.replace('},','}\n')
+        df_nested_enhancedPP['enhancedPlusPlusDependencies'] = df_nested_enhancedPP['enhancedPlusPlusDependencies'].astype('str').str.replace('},','}\n')
+        df_nested_entity['entitymentions'] = df_nested_entity['entitymentions'].astype('str').str.replace('},','}\n')
+        df_nested_tokens['tokens'] = df_nested_tokens['tokens'].astype('str').str.replace('},','}\n')
         
+        return [output_nlp(Json_data, tokn, Pos, Ner, Parse, Dep_parse), tabulate(df_nested_3, headers='keys', tablefmt='psql'),
+                tabulate(df_nested_basic, headers='keys', tablefmt='psql'), tabulate(df_nested_enhanced, headers='keys', tablefmt='psql'),
+                tabulate(df_nested_enhancedPP, headers='keys', tablefmt='psql'),
+                tabulate(df_nested_entity, headers='keys', tablefmt='psql'), tabulate(df_nested_tokens, headers='keys', tablefmt='psql')]
+"""
+                tabulate(df_nested_Sen["basicDependencies"], headers='keys', tablefmt='psql'),
+                tabulate(df_nested_Sen["enhancedDependencies"], headers='keys', tablefmt='psql'),
+                tabulate(df_nested_Sen["enhancedPlusPlusDependencies"], headers='keys', tablefmt='psql'),
+                tabulate(df_nested_Sen["entitymentions"], headers='keys', tablefmt='psql'),
+                tabulate(df_nested_Sen["tokens"], headers='keys', tablefmt='psql')]
+"""        
 
+# def output_nlp(Json_data, tokn, Pos, Ner, Parse, Dep_parse):
+#     parse_tree = ParentedTree.fromstring(Parse)
+#     parse_tree.pretty_print()
+#     return "NLP Parse: \n {}".format(Parse)
+
+def output_nlp(Json_data, tokn, Pos, Ner, Parse, Dep_parse):
+    parse_tree = ParentedTree.fromstring(Parse)
+    pretty_tree = TreePrettyPrinter(parse_tree).text()
+    tree = pretty_tree.split("\n")
+    tree.reverse()
+    parse_print = '\n'.join(tree)
+    parse_print = re.sub("_", "-", parse_print)
+    return "NLP Parse(sentence structure): \n{}".format(parse_print)
+
+    
 if __name__ == "__main__":
 
     app.run_server(debug=False)
